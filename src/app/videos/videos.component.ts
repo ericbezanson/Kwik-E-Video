bf6bd06f0477b6ff1dcc20b4fd37befb0af97222
Matt Wright <matt_182@live.com>
2017-11-04 14:20:23 -0300

    Temp push for Evan

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-videos',
@@ -11,18 +12,53 @@ export class VideosComponent{
     {
       title: "Title 1",
       imageUrl: "http://via.placeholder.com/350x500",
-      description: "Sample desc 1"
+      description: "Sample desc 1",
+      genre: "Horror"    
     },
     {
       title: "Title 2",
       imageUrl: "http://via.placeholder.com/350x500",
-      description: "Sample desc 2"
+      description: "Sample desc 2",
+      genre: "Action"    
     },
   ]
 
-  constructor() { 
+  filteredVideos;
+
+  genre: string;
+
+  genres = [
+    {
+      name: "All"
+    },
+    {
+      name: "Comedy"
+    },
+    {
+      name: "Horror"
+    },
+    {
+      name: "Action"
+    },
+  ]
+
+  constructor(route: ActivatedRoute) { 
     // Have to retrieve list of videos here
     // Discuss with Evan
+    this.filteredVideos = this.videos;
+    
+    // Retireve genre filter
+    /*
+    route.queryParamMap.subscribe(params => {
+      this.genre = params.get('genre');
+      
+      /*
+      this.filteredVideos = (this.genre) ?
+        this.videos.filter(v => v.genre === this.genre) :
+        this.videos
+        
+    }); 
+    */   
   }
 
 }
