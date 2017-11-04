36d7a6b721909f199dba2f69afcd1e39dca81698
Matt Wright <matt_182@live.com>
2017-11-04 15:08:09 -0300

    Fixed commit mistake, model should be pushed now

---
+++ src/app/models/video.ts
@@ -0,0 +1,6 @@
+export interface Video {
+    title: string;
+    imageUrl: string;
+    description: string;
+    genre: string;
+}
--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -24,7 +24,7 @@ export class VideosComponent{
     },
   ]
 
-  filteredVideos;
+  filteredVideos: Video[];
 
   genre: string;
 
